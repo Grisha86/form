1885b89213791de7f68cca41e5ee87788e7ed735
Grisha86 <Grixamail@rambler.ru>
2015-06-14 21:25:02 +0300

    nixrena ne pon

--- grisha.php
+++ grisha.php
@@ -2,9 +2,26 @@
 session_start();
 include 'users.php';
 
-if ($_SESSION['username']=="grisha") {
+if ($_SESSION['username'] == "grisha") {
     $logeduser = $users[$_SESSION['username']];
     echo "Hello ".$logeduser['name'];
+    //$host = 'localhost';
+    //$db = 'db_users';
+    $userdb = 'root';
+    $pasuserdb = '123456';
+    //$charset=??
+    $dsn = "mysql:host = localhost; dbname = db_users";
+    $opt = array(
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    );
+    $pdo = new PDO($dsn,  $userdb, $pasuserdb, $opt);
+    $stmt = $pdo->prepare('SELECT name FROM db_users WHERE name = ?');
+    $stmt->execute([$_GET['email']]);
+    foreach ($stmt as $row)
+    {
+        echo $row['name'] . "\n";
+    }
     echo "<form action='form.php' method='post'>
             <input type='submit' value='Log out' name='out'>
           </form>
