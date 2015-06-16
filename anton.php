64e09f39d8d28015fb2e79165b413139440e2799
Grisha86 <Grixamail@rambler.ru>
2015-06-16 21:26:50 +0300

    ??

--- anton.php
+++ anton.php
@@ -9,11 +9,15 @@ if ($_SESSION['username']=="anton") {
     $password = '123456';
     $db = new PDO($dataSource, $user, $password);
 //$db->exec("INSERT INTO users (login, firstname, lastname, password, vk_url ) VALUES ('serg', 'Sergey', 'Ivashchenko', '$2y$10$1O6yO0jZ9zzNzzE0v.rlPeEeSthC7E1KEAE2.AM83xUwutz/RYYXi', 'http://vk.com/mantichoras')");
-    $dblogin = 'grisha';
-    $stmt = $db->prepare('SELECT name FROM users_table WHERE name=:name' );
+    $dblogin = 'anton';
+    $stmt = $db->prepare('SELECT * FROM users_table WHERE name=:name' );
     $stmt->execute(array(':name' => $dblogin,));
-//$result = $db->query("SELECT * FROM users WHERE login=$dblogin");
     $row = $stmt->fetch();
+    //$stmt = $db->query('SELECT name FROM users');
+    //while ($row = $stmt->fetch())
+    // {
+    //     echo $row['name'] . "\n";
+    // }
     var_dump($row);
     echo "<form action='form.php' method='post'>
             <input type='submit' value='Log out' name='out'>
--- grisha.php
+++ grisha.php
@@ -9,19 +9,17 @@ if ($_SESSION['username'] == "grisha") {
     //$db = 'db_users';
     $userdb = 'root';
     $pasuserdb = '123456';
-    //$charset=??
-    $dsn = "mysql:host = localhost; dbname = db_users";
-    $opt = array(
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    );
-    $pdo = new PDO($dsn,  $userdb, $pasuserdb, $opt);
-    $stmt = $pdo->prepare('SELECT name FROM db_users WHERE name = ?');
-    $stmt->execute([$_GET['email']]);
-    foreach ($stmt as $row)
-    {
-        echo $row['name'] . "\n";
-    }
+    $dsn = "mysql:dbname = db_users; host = localhost ";
+    //$opt = array(
+    //    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    //    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    //);
+    $pdo = new PDO($dsn,  $userdb, $pasuserdb);
+    $dblogin = 'grisha';
+    $stmt = $pdo->prepare('SELECT * FROM users_table WHERE name=:name' );
+    $stmt->execute(array(':name' => $dblogin,));
+    $row = $stmt->fetch();
+    var_dump($row);
     echo "<form action='form.php' method='post'>
             <input type='submit' value='Log out' name='out'>
           </form>
